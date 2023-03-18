c7e1cba94c55d7b0852a4fc2b7512ac87760278d
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-03-18 08:42:49 -0400

    Added event, gender and age selection ui

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import './App.css';
 import ega from './ega.png';
 
+//Header of app
 const Header = () => {
   return (
-    <div className="main-container">
+    <div className="header-container">
       <img src={ega} alt='EGA' height={50}/>
       <h1>USMC PFT/CFT Calculator</h1>
       <img src={ega} alt='EGA' height={50}/>
@@ -12,9 +13,46 @@ const Header = () => {
   );
 }
 
+//Section to select to calculate a PFT or CFT
+const PFTorCFT = () => {
+
+  const handleClick = () => {
+    console.log('click');
+  }
+
+  return (
+    <div className="pft-or-cft-container">
+      <button onClick={handleClick} className="pftcft-button">PFT</button>
+      <button onClick={handleClick} className="pftcft-button">CFT</button>
+    </div>
+  );
+}
+
+//Section to select gender and age
+const MFAge = () => {
+  return(
+    <div className="mfage">
+      <button className="male-female-button">MALE</button>
+      <button className="male-female-button">FEMALE</button>
+      <input className="age-input" type='number' name='age' placeholder='Age'/>
+    </div>
+  );
+}
+
+//  *** PFT EVENTS ***
+const PushupsPullups = () => {
+  
+}
+
+
+
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <PFTorCFT />
+      <MFAge />
+    </>
   );
 } 
 
