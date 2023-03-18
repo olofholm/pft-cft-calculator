e61a5256e58848ec8354da1f29e609dac34af9d7
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-03-18 07:53:12 -0400

    Added header

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import './App.css';
+import ega from './ega.png';
+
+const Header = () => {
+  return (
+    <div className="main-container">
+      <img src={ega} alt='EGA' height={50}/>
+      <h1>USMC PFT/CFT Calculator</h1>
+      <img src={ega} alt='EGA' height={50}/>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <h1>USMC PFT/CFT Calculator</h1>
+    <Header />
   );
-}
+} 
 
 export default App;
