80da9c188f74490279161adb0409ea832fbb4d6e
olofholm <olof.fredrick.holmstrom@gmail.com>
2023-03-18 10:20:11 -0400

    Create UI for PFT events

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import TimerPicker from 'react-time-picker';
 import ega from './ega.png';
 
 //Header of app
@@ -41,7 +42,45 @@ const MFAge = () => {
 
 //  *** PFT EVENTS ***
 const PushupsPullups = () => {
-  
+
+  return (
+    <div className="event-container">
+      <h2>Pull-Ups/Push-Ups</h2>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '10px'}}>
+        <select>
+          <option value='pullups'>Pull-Ups</option>
+          <option value='pushups'>Push-Ups</option>
+        </select>
+        <input type='number' placeholder='Reps' className="reps"></input>
+      </div>
+    </div>
+  );
+}
+
+const Plank = () => {
+  return (
+    <div className="event-container">
+      <h2>Plank</h2>
+      <TimerPicker format="mm:ss" disableClock={true} className='plank-time'/>
+    </div>
+  );
+}
+
+const RunningRowing = () => {
+  return (
+    <div className="event-container">
+      <h2>Running/Rowing</h2>
+      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '10px'}}>
+        <select>
+          <option value='3milerun'>3 Mile Run</option>
+          <option value='3milerunAlt'>3 Mile Run (Altitude)</option>
+          <option value='5kRow'>5K Rowing</option>
+          <option value='5kRowAlt'>5K Rowing (Altitude)</option>
+        </select>
+        <TimerPicker format="mm:ss" disableClock={true} className='plank-time'/>
+      </div>
+    </div>
+  );
 }
 
 
@@ -52,6 +91,9 @@ function App() {
       <Header />
       <PFTorCFT />
       <MFAge />
+      <PushupsPullups />
+      <Plank />
+      <RunningRowing />
     </>
   );
 } 
